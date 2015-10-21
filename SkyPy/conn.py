b540685a38dad3b20dec57e38bf92bd0772f7c68
Ollie Terrance <ollie.terrance@live.co.uk>
2015-10-21 23:09:53 +0100

    Cleanup token hash function a bit more

--- SkyPy/conn.py
+++ SkyPy/conn.py
@@ -171,17 +171,6 @@ class SkypeApiException(Exception):
     pass
 
 def getMac256Hash(challenge, appId, key):
-    def padRight(original, totalWidth, ch):
-        def stringFromChar(ch, count):
-            s = ch
-            for i in range(1, count):
-                s += ch
-            return s
-        if len(original) < totalWidth:
-            ch = ch or " "
-            return original + stringFromChar(ch, totalWidth - len(original))
-        else:
-            return original.valueOf()
     def int32ToHexString(n):
         hexChars = "0123456789abcdef"
         hexString = ""
@@ -206,7 +195,7 @@ def getMac256Hash(challenge, appId, key):
         for i in range(len(sA)):
             sC += "0" if sA[i] == sB[i] else "1"
         return int(sC, 2)
-    def cS64_C(pdwData, pInHash):
+    def cS64(pdwData, pInHash):
         if len(pdwData) < 2 or len(pdwData) & 1 == 1:
             return None
         MODULUS = 2147483647
@@ -241,38 +230,20 @@ def getMac256Hash(challenge, appId, key):
         qwSum = qwSum % MODULUS
         return [qwMAC, qwSum]
     clearText = challenge + appId
-    remaining = 8 - len(clearText) % 8
-    if remaining != 8:
-        clearText = padRight(clearText, len(clearText) + remaining, "0")
+    clearText += "0" * (8 - len(clearText) % 8)
     cchClearText = len(clearText) // 4
     pClearText = []
-    pos = 0
     for i in range(cchClearText):
         pClearText = pClearText[:i] + [0] + pClearText[i:]
-        pClearText[i] += ord(clearText[pos]) * 1
-        pos += 1
-        pClearText[i] += ord(clearText[pos]) * 256
-        pos += 1
-        pClearText[i] += ord(clearText[pos]) * 65536
-        pos += 1
-        pClearText[i] += ord(clearText[pos]) * 16777216
-        pos += 1
+        for pos in range(4):
+            pClearText[i] += ord(clearText[pos]) * (256 ** pos)
     sha256Hash = [0, 0, 0, 0]
     hash = hashlib.sha256((challenge + key).encode("utf-8")).hexdigest().upper()
-    pos = 0
     for i in range(len(sha256Hash)):
         sha256Hash[i] = 0
-        sha256Hash[i] += int(hash[pos:pos+2], 16) * 1
-        pos += 2
-        sha256Hash[i] += int(hash[pos:pos+2], 16) * 256
-        pos += 2
-        sha256Hash[i] += int(hash[pos:pos+2], 16) * 65536
-        pos += 2
-        sha256Hash[i] += int(hash[pos:pos+2], 16) * 16777216
-        pos += 2
-    macHash = cS64_C(pClearText, sha256Hash)
-    a = int64Xor(sha256Hash[0], macHash[0])
-    b = int64Xor(sha256Hash[1], macHash[1])
-    c = int64Xor(sha256Hash[2], macHash[0])
-    d = int64Xor(sha256Hash[3], macHash[1])
-    return int32ToHexString(a) + int32ToHexString(b) + int32ToHexString(c) + int32ToHexString(d)
+        for pos in range(4):
+            dpos = pos * 2
+            sha256Hash[i] += int(hash[dpos:dpos + 2], 16) * (256 ** pos)
+    macHash = cS64(pClearText, sha256Hash)
+    macParts = [macHash[0], macHash[1], macHash[0], macHash[1]]
+    return "".join(map(int32ToHexString, map(int64Xor, sha256Hash, macParts)))
