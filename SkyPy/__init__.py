42d9c755b18e7a0e49cb234e738e6bb33b70dda9
Ollie Terrance <ollie.terrance@live.co.uk>
2016-01-21 20:34:17 +0000

    Add missing classes to module __init__

--- SkyPy/__init__.py
+++ SkyPy/__init__.py
@@ -1,8 +1,10 @@
 from SkyPy.core import Skype, SkypeEventLoop
 from SkyPy.conn import SkypeConnection, SkypeEndpoint, SkypeAuthException
 from SkyPy.user import SkypeUser, SkypeContact, SkypeContacts, SkypeRequest
-from SkyPy.chat import SkypeChat, SkypeSingleChat, SkypeGroupChat
-from SkyPy.msg import SkypeMsg, SkypeContactMsg, SkypeFileMsg, SkypeImageMsg
-from SkyPy.event import SkypeEvent, SkypePresenceEvent, SkypeEndpointEvent, SkypeTypingEvent, SkypeMessageEvent, \
-                        SkypeNewMessageEvent, SkypeEditMessageEvent, SkypeChatUpdateEvent
+from SkyPy.chat import SkypeChat, SkypeSingleChat, SkypeGroupChat, SkypeChats
+from SkyPy.msg import SkypeMsg, SkypeContactMsg, SkypeFileMsg, SkypeImageMsg, SkypeCallMsg, SkypeMemberMsg, \
+                      SkypeAddMemberMsg, SkypeRemoveMemberMsg
+from SkyPy.event import SkypeEvent, SkypePresenceEvent, SkypeEndpointEvent, SkypeTypingEvent, \
+                        SkypeMessageEvent, SkypeNewMessageEvent, SkypeEditMessageEvent, SkypeCallEvent, \
+                        SkypeChatUpdateEvent, SkypeChatMemberEvent
 from SkyPy.util import SkypeException, SkypeApiException
