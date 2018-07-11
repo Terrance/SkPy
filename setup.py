0f9489c94e8ec4d3effab4314497428872a80ad1
Ollie Terrance <ollie.terrance@live.co.uk>
2018-07-11 20:36:08 +0100

    Promote to beta, add test requirements

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from os import path
 setup(name="SkPy",
       description="An unofficial Python library for interacting with the Skype HTTP API.",
       long_description=open(path.join(path.abspath(path.dirname(__file__)), "README.rst"), "r").read(),
-      author="Ollie Terrance",
       packages=["skpy"],
       install_requires=["beautifulsoup4", "requests"],
-      classifiers=["Development Status :: 3 - Alpha",
+      tests_require=["beautifulsoup4", "requests", "responses"],
+      classifiers=["Development Status :: 4 - Beta",
                    "Intended Audience :: Developers",
                    "Topic :: Communications :: Chat",
                    "Topic :: Software Development :: Libraries"])
