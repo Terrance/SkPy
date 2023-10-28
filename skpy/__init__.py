00797b16a656ae13355c356d514feb0bdeb40579
Ollie Terrance <ollie.terrance@live.co.uk>
2023-10-28 08:49:01 +0100

    Add missing top-level imports
    Fixes #246.

--- skpy/__init__.py
+++ skpy/__init__.py
@@ -6,11 +6,13 @@ from skpy.core import SkypeObj, SkypeObjs, SkypeEnum, SkypeException, SkypeApiEx
 from skpy.util import SkypeUtils
 from skpy.main import Skype, SkypeEventLoop, SkypeSettings, SkypeTranslator
 from skpy.conn import SkypeConnection, SkypeAuthProvider, SkypeAPIAuthProvider, SkypeLiveAuthProvider, \
-                      SkypeSOAPAuthProvider, SkypeRefreshAuthProvider, SkypeGuestAuthProvider, SkypeEndpoint
+                      SkypeSOAPAuthProvider, SkypeGuestAuthProvider, SkypeRefreshAuthProvider, \
+                      SkypeRegistrationTokenProvider, SkypeEndpoint
 from skpy.user import SkypeUser, SkypeContact, SkypeBotUser, SkypeContacts, SkypeContactGroup, SkypeRequest
 from skpy.chat import SkypeChat, SkypeSingleChat, SkypeGroupChat, SkypeChats
 from skpy.msg import SkypeMsg, SkypeTextMsg, SkypeContactMsg, SkypeLocationMsg, SkypeCardMsg, \
-                     SkypeFileMsg, SkypeImageMsg, SkypeCallMsg, SkypeMemberMsg, \
+                     SkypeFileMsg, SkypeImageMsg, SkypeAudioMsg, SkypeVideoMsg, SkypeCallMsg, SkypePropertyMsg, \
+                     SkypeTopicPropertyMsg, SkypeOpenPropertyMsg, SkypeHistoryPropertyMsg, SkypeMemberMsg, \
                      SkypeAddMemberMsg, SkypeChangeMemberMsg, SkypeRemoveMemberMsg
 from skpy.event import SkypeEvent, SkypePresenceEvent, SkypeEndpointEvent, SkypeTypingEvent, \
                        SkypeMessageEvent, SkypeNewMessageEvent, SkypeEditMessageEvent, SkypeCallEvent, \
